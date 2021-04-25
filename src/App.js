a3570c8659898314702974f95a2d01b19fbfe930
Eduardo Querido <eduardo.querido@znc.com.br>
2021-04-25 10:28:40 -0300

    Começando a adicionar a API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/HomeScreen';
-import LoginScreen from './src/LoginScreen';
-import CreateUserScreen from './src/CreateUserScreen';
+import HomeScreen from './HomeScreen';
+import LoginScreen from './LoginScreen';
+import CreateUserScreen from './CreateUserScreen';
 
 // ------------------------APP----------------------------------
 
